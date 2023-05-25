af01d9386c4b6121799870c6b1f0c2a046379852
Jonathan Booker <97480950+JonathanBooker912@users.noreply.github.com>
2023-05-24 19:11:11 -0500

    Added Player Information Form
    Still needs work, Just pushing code

--- src/router.js
+++ src/router.js
@@ -8,6 +8,8 @@ import AddTutorial from "./views/AddTutorial.vue";
 import ViewTutorial from "./views/ViewTutorial.vue";
 import AddLesson from "./views/AddLesson.vue";
 import EditLesson from "./views/EditLesson.vue";
+import AdminDashboard from "./views/AdminDashboard.vue";
+import EditPlayerInfo from "./views/EditPlayerInfo.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,6 +19,18 @@ const router = createRouter({
       alias: "/login",
       name: "login",
       component: Login,
+    },
+    {
+      path: "/",
+      alias: "/adminDashboard",
+      name: "adminDashboard",
+      component: AdminDashboard,
+    },
+    {
+      path: "/",
+      alias: "/playerForm",
+      name: "playerForm",
+      component: EditPlayerInfo,
     },
     {
       path: "/tutorials",
