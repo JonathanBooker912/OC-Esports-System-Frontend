90350176818d247f2f6547cffaad8fc0e7919f9e
KyleD0711 <97480794+KyleD0711@users.noreply.github.com>
2023-07-15 16:48:44 -0500

    Completed view players frontend changes
    Add a basic view page

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,10 +1,15 @@
 import apiClient from "./services";
 
 export default {
-  getAll() {
-    return apiClient.get("/user");
+  getAll(pageSize, page) {
+    return apiClient.get(`/user?pageSize=${pageSize}&page=${page}`);
   },
   get(id) {
     return apiClient.get("/user/" + id);
+  },
+  search(filter, pageSize, page) {
+    return apiClient.get(
+      `/user?filter=${filter}&pageSize=${pageSize}&page=${page}`
+    );
   },
 };
--- src/views/EditUsers.vue
+++ src/views/EditUsers.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import userServices from "../services/userServices";
+import { useVuelidate } from "@vuelidate/core";
+import { required, email } from "@vuelidate/validators";
 
 const user = ref({});
 const props = defineProps({
@@ -14,7 +16,6 @@ function getPlayerInfo() {
     .get(props.userId)
     .then((response) => {
       user.value = response.data;
-      console.log(user.value);
     })
     .catch((err) => {
       console.log(err);
@@ -27,6 +28,31 @@ onMounted(() => {
 </script>
 <template>
   <div>
-    <!--Need to create a form for this-->
+    <v-form class="container">
+      <v-text-field v-model="user.fName" label="First Name"></v-text-field>
+      <v-text-field v-model="user.lName" label="Last Name"></v-text-field>
+      <v-text-field
+        v-model="user.phoneNumber"
+        label="Phone Number"
+      ></v-text-field>
+      <v-text-field v-model="user.email" label="Email"></v-text-field>
+      <v-text-field v-model="user.address" label="Address"></v-text-field>
+      <v-select v-model="user.shirtSize" label="Shirt Size"></v-select>
+      <v-select v-model="user.pantSize" label="Pant Size"></v-select>
+      <v-checkbox v-model="user.outsidePC" label="Outside PC?"></v-checkbox>
+      <v-checkbox v-model="user.fullVacc" label="Full Vacc"></v-checkbox>
+      <v-text-field
+        v-model="user.classification"
+        label="Classification"
+      ></v-text-field>
+      <v-text-field v-model="user.role" label="Role"></v-text-field>
+      <v-btn></v-btn>
+    </v-form>
   </div>
 </template>
+<style scoped>
+.container {
+  width: 70%;
+  height: 70%;
+}
+</style>
